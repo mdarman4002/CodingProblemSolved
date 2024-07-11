749fefbf31a8d1fb0b4d60978e9b81ec5637a14f
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-07-11 22:54:02 +0530

    Create 155. Min Stack.java

---
+++ Stack/155. Min Stack.java
@@ -0,0 +1,87 @@
+class MinStack {
+   Stack<Integer> stack;
+   Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack();
+        minStack = new Stack();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if( minStack.isEmpty() || minStack.peek() > val){
+            minStack.push(val);
+        }else{
+            minStack.push(minStack.peek());
+        }
+
+    }
+    
+    public void pop() {
+        stack.pop(); minStack.pop();
+       
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+class MinStack {
+
+    private Node head;
+        
+    public void push(int x) {
+        if (head == null) 
+            head = new Node(x, x, null);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+        
+    private class Node {
+        int val;
+        int min;
+        Node next;
+            
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
