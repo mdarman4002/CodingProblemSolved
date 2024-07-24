f5527b15f441c238b3c335372ba64fe057880a33
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-07-24 15:00:15 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- Array/121. Best Time to Buy and Sell Stock.java
+++ Array/121. Best Time to Buy and Sell Stock.java
@@ -12,3 +12,25 @@ class Solution {
         return maxProfit;
     }
 }
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0; int min = prices[0];
+        int len = prices.length;
+        if(len < 2){
+            return 0;
+        }
+        for(int i = 0; i < len; i++){
+            int cost = prices[i] - min;
+            max = Math.max(max, cost);
+            min = Math.min(min, prices[i]);
+        }return max;
+        // for(int i = 0; i < len-1; i++){
+        //     for(int j = i+1; j < len; j++){
+        //         max = Math.max(max,prices[j] - prices[i]);
+        //     }
+        // }return max; // time limit exceeded;
+    }
+}
+
