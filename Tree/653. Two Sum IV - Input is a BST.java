6374f0f55d5d4dc8c9609ad4f69cca8ade2919ae
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-07-25 19:10:56 +0530

    reduce the time complexity

--- Tree/653. Two Sum IV - Input is a BST.java
+++ Tree/653. Two Sum IV - Input is a BST.java
@@ -7,7 +7,7 @@
  * TreeNode() {}
  * TreeNode(int val) { this.val = val; }
  * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
+ * this.val = val; 
  * this.left = left;
  * this.right = right;
  * }
