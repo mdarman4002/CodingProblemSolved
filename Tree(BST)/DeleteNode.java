2a1cc9452416345ee5158ce9c398a54320f79063
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-08-27 02:13:51 +0530

    Update DeleteNode.java

--- Tree(BST)/DeleteNode.java
+++ Tree(BST)/DeleteNode.java
@@ -1,5 +1,9 @@
 public class DeleteNode {
     
+    /**
+     * Main method to test the delete node functionality
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         int[] nodes = {5, 1, 3, 4, 2, 7};
         Node root = null;
@@ -14,19 +18,31 @@ public class DeleteNode {
         inorder(root);
        
     }
+    
+    /**
+     * Deletes a node with the given value from the binary search tree
+     * @param root the root of the binary search tree
+     * @param val the value to be deleted
+     * @return the updated root of the binary search tree
+     */
     public static Node delete(Node root, int val){
+        if(root == null){
+            return null; // tree is empty, nothing to delete
+        }
+        
         if(root.data < val){
+            // value to be deleted is in the right subtree
             root.right = delete(root.right, val);
         }else if(root.data > val){
+            // value to be deleted is in the left subtree
             root.left = delete(root.left, val);
-        }
-        else{// case 1:- if root.left and root.right both are null
+        }else{// case 1:- if root.left and root.right both are null
             // leaf node
             if(root.left == null && root.right==null && root.data == val){
                 return null;
             }
             // case 2:- if either root.left or  root.right is null then return opposite one
-            // sinle child
+            // single child
             else if(root.left == null && root.data == val){
                 return root.right;
             }else if(root.right == null && root.data == val){
@@ -36,11 +52,14 @@ public class DeleteNode {
             Node inorderSuccessor = findInorderSuccessor(root.right);
             root.data = inorderSuccessor.data;
             root.right = delete(root.right, inorderSuccessor.data); 
-
-
         }
         return root;
     }
+    
+    /**
+     * Performs an in-order traversal of the binary search tree and prints the node values
+     * @param root the root of the binary search tree
+     */
     public static void inorder(Node root) {
         if (root == null) {
             return;
@@ -49,6 +68,12 @@ public class DeleteNode {
         System.out.print(root.data + " ");
         inorder(root.right);
     }
+    
+    /**
+     * Finds the in-order successor of a node in the binary search tree
+     * @param root the root of the binary search tree
+     * @return the in-order successor node
+     */
     public static Node findInorderSuccessor(Node root){
         while(root.left != null){
             root = root.left;
@@ -56,6 +81,9 @@ public class DeleteNode {
         return root;
     }
     
+    /**
+     * Node class representing a node in the binary search tree
+     */
     public static class Node {
         int data;
         Node left;
@@ -65,6 +93,12 @@ public class DeleteNode {
             this.data = data;
         }
 
+        /**
+         * Inserts a new node with the given value into the binary search tree
+         * @param root the root of the binary search tree
+         * @param n the value to be inserted
+         * @return the updated root of the binary search tree
+         */
         public static Node insert(Node root, int n) {
             if (root == null) {
                 root = new Node(n);
@@ -79,4 +113,4 @@ public class DeleteNode {
             return root;
         }
     } 
-} 
+}
