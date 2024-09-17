b7dd1b3018250209ad7beefeaca28e79bfe28cf4
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-09-18 00:36:54 +0530

    Update 2085. Count Common Words With One Occurrence.java
    updated code with better accuracy.

--- Hash/2085. Count Common Words With One Occurrence.java
+++ Hash/2085. Count Common Words With One Occurrence.java
@@ -24,3 +24,34 @@ class Solution {
         return count;
     }
 }
+-------------------------------update code with high efficiency--------------------------------------------------
+
+    class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        HashMap<String, Integer> map1 = new HashMap<>();
+
+        for (String word : words1) {
+            if (map1.containsKey(word)) {
+                map1.put(word, map1.getOrDefault(word, 0) + 1);
+            } else {
+                map1.put(word, 1);
+            }
+        }
+
+        for (String word2 : words2) {
+            if (map1.containsKey(word2)) {
+                if (map1.get(word2) <= 1) {
+                    map1.put(word2, map1.get(word2) - 1);
+                }else{
+                    map1.put(word2, map1.get(word2)+1);
+                }
+            } 
+        }
+        int count = 0;
+      Set<String> set = map1.keySet();
+        for (String ele : set) {
+            if (map1.get(ele) == 0) {
+                count++;
+            }
+        }
+        return count;
