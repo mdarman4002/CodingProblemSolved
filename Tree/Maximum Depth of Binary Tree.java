d71edb676d8fbdace1b4baf241ea0034fce2bc9e
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-10-08 01:59:08 +0530

    104. Maximum Depth of Binary Tree.java

--- Tree/Maximum Depth of Binary Tree.java
+++ Tree/Maximum Depth of Binary Tree.java
@@ -21,11 +21,12 @@ class Solution {
             int leftDepth = maxDepth(root.left);
             int rightDepth = maxDepth(root.right);
 
-            if(leftDepth > rightDepth){
-                return leftDepth + 1;
-            }else{
-                return rightDepth + 1;
-            }
+            return Math.max(leftDepth , rightDepth)+1;
+            // if(leftDepth > rightDepth){
+            //     return leftDepth + 1;
+            // }else{
+            //     return rightDepth + 1;
+            // }
         }
     }
 }
